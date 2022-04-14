78855052da662b5e97153e34f50788be6fce10b5
seleb0nkina <kaselivanova_1@edu.hse.ru>
2022-04-14 19:19:10 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include "monomial.h"
 
+int main() {
+    Monomial<int> m(1, {{0, 1},
+                        {1, 2}});
+
+    std::cout << m << std::endl; // ожидается 1x_0^1x_1^2
+
+    Monomial<int> a(1, {{0, 1},
+                        {1, 1}});
+    Monomial<int> b(7, {{2, 9}});
+    std::cout << a << ' ' << b << std::endl;//ожидается 1x_0^1x_1^1 7x_2^9
+    std::cout << a * b << std::endl;// ожидается 7x_0^1x_1^1x_2^9
+    a *= b;
+    std::cout << a << std::endl;// ожидается 7x_0^1x_1^1x_2^9
+    Monomial<int> c(9, {{3, 1}});
+    a /= b;
+    std::cout << a << std::endl;// ожидается 1x_0^1x_1^1
+
+    return 0;
+}
---
+++ monomial.h
@@ -0,0 +1,110 @@
+#ifndef MONOMIAL_H
+#define MONOMIAL_H
+
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+template<typename T>
+class Monomial {
+public:
+    using IndexType = unsigned int;
+    using DegreeType = int;
+    using PowersType = std::map<IndexType, DegreeType>;
+
+    Monomial() = default;
+
+    Monomial(const T& coefficient, std::initializer_list<std::pair<IndexType, DegreeType>> l) {
+        coefficient_ = coefficient;
+        for(const auto &j : l) {
+            powers_[j.first] = j.second;
+        }
+    }
+
+    Monomial &operator*=(const Monomial &other) {
+        coefficient_ *= other.coefficient_;
+        for (const auto &j: other.powers_) {
+            powers_[j.first] += j.second;
+        }
+        return *this;
+    }
+
+    bool isDivisibleBy(const Monomial &other) const {
+        for(const auto &j : other.powers_) {
+            auto power = powers_.find(j.first);
+            if(power == powers_.end() || power->second < j.second) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Monomial &operator/=(const Monomial &other) {
+        if(!isDivisibleBy(other)) {
+            //
+        }
+        coefficient_ /= other.coefficient_;
+        for (const auto &j: other.powers_) {
+            auto power = powers_.find(j.first);
+
+            power->second -= j.second;
+            if (power->second == 0) {
+                powers_.erase(power);
+            }
+        }
+        assert(isStateCorrect());
+
+        return *this;
+    }
+
+    Monomial operator*(const Monomial &other) const {
+        Monomial temp = *this;
+        temp *= other;
+        return temp;
+    }
+
+    Monomial operator/(const Monomial &other) const {
+        Monomial temp = *this;
+        temp /= other;
+        return temp;
+    }
+
+    Monomial &operator=(const Monomial &other) {
+        coefficient_ = other.coefficient_;
+        powers_ = other.powers_;
+        return *this;
+    }
+
+    T getCoefficient() const {
+        return coefficient_;
+    }
+
+    PowersType getPowers() const {
+        return powers_;
+    }
+
+    bool isStateCorrect() const {
+        for (const auto& p : powers_) {
+            if (p.second <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Monomial &m) {
+        out << m.coefficient_;
+        for (const auto &p: m.powers_) {
+            out << "x_" << p.first << "^" << p.second;
+        }
+        return out;
+    }
+
+private:
+    T coefficient_ = T(0);
+    PowersType powers_;
+};
+
+#endif // MONOMIAL_H
