b578617d92ec711082a7f3661ffd4d609591cb77
seleb0nkina <kaselivanova_1@edu.hse.ru>
2022-04-19 17:24:16 +0300

    added CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include "monomial.h"
+#include <boost/rational.hpp>
 
 int main() {
-    Monomial<int> m(1, {{0, 1},
+    Monomial<boost::rational<int>> m(1, {{0, 1},
                         {1, 2}});
 
     std::cout << m << std::endl; // ожидается 1x_0^1x_1^2
 
-    Monomial<int> a(1, {{0, 1},
+    Monomial<boost::rational<int>> a(1, {{0, 1},
                         {1, 1}});
-    Monomial<int> b(7, {{2, 9}});
+    Monomial<boost::rational<int>> b(7, {{2, 9}});
     std::cout << a << ' ' << b << std::endl;//ожидается 1x_0^1x_1^1 7x_2^9
     std::cout << a * b << std::endl;// ожидается 7x_0^1x_1^1x_2^9
     a *= b;
     std::cout << a << std::endl;// ожидается 7x_0^1x_1^1x_2^9
-    Monomial<int> c(9, {{3, 1}});
+    Monomial<boost::rational<int>> c(9, {{3, 1}});
     a /= b;
     std::cout << a << std::endl;// ожидается 1x_0^1x_1^1
-
+    Monomial<boost::rational<int>> d(4, {});
+    Monomial<boost::rational<int>> e(6,{});
+    std::cout<<d/e<<std::endl;
     return 0;
 }
